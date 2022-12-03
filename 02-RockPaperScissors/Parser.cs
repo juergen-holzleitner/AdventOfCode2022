cec3c94ee312b86d80ada5c24501b06f88a40439
Juergen Holzleitner <juergen@juergen-holzleitner.com>
2022-12-03 10:52:15 +0100

    add parsing of shapes

---
+++ 02-RockPaperScissors/Parser.cs
@@ -0,0 +1,31 @@
+﻿namespace _02_RockPaperScissors
+{
+  public static class Parser
+  {
+    public enum Shape { Rock, Paper, Scissors };
+
+    public static Shape ParseOpponent(char shapeInput)
+    {
+      if (shapeInput == 'A')
+        return Shape.Rock;
+      if (shapeInput == 'B')
+        return Shape.Paper;
+      if (shapeInput == 'C')
+        return Shape.Scissors;
+
+      throw new ArgumentException($"{nameof(shapeInput)} has invalid value {shapeInput}");
+    }
+
+    public static Shape ParsePlayer(char shapeInput)
+    {
+      if (shapeInput == 'X')
+        return Shape.Rock;
+      if (shapeInput == 'Y')
+        return Shape.Paper;
+      if (shapeInput == 'Z')
+        return Shape.Scissors;
+
+      throw new ArgumentException($"{nameof(shapeInput)} has invalid value {shapeInput}");
+    }
+  }
+}
--- 02-RockPaperScissors/RockPaperScissorsTest.cs
+++ 02-RockPaperScissors/RockPaperScissorsTest.cs
@@ -1,11 +1,29 @@
+using FluentAssertions;
+
 namespace _02_RockPaperScissors
 {
   public class RockPaperScissorsTest
   {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData('A', Parser.Shape.Rock)]
+    [InlineData('B', Parser.Shape.Paper)]
+    [InlineData('C', Parser.Shape.Scissors)]
+    public void Can_parse_Opponent(char inputShape, Parser.Shape result)
     {
+      var shape = Parser.ParseOpponent(inputShape);
 
+      shape.Should().Be(result);
+    }
+
+    [Theory]
+    [InlineData('X', Parser.Shape.Rock)]
+    [InlineData('Y', Parser.Shape.Paper)]
+    [InlineData('Z', Parser.Shape.Scissors)]
+    public void Can_parse_Player(char inputShape, Parser.Shape result)
+    {
+      var shape = Parser.ParsePlayer(inputShape);
+
+      shape.Should().Be(result);
     }
   }
 }
